f40707e5d34ddae7e35da2e6a723ff8c0ecfb011
blurhack <blurhack2332@gmail.com>
2026-06-16 19:32:31 +0530

    fix craco webpack alias for @/ paths

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,4 +1,16 @@
+const path = require('path');
+const { addWebpackAlias, override } = require('customize-cra');
+
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+      };
+      return webpackConfig;
+    },
+  },
   style: {
     postcss: {
       plugins: [
